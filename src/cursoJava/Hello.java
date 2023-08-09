0ff5c266e6e072bd7d767d005cd53852f2df53ae
anarsantos <aninha_rsantos@yahoo.com.br>
2023-08-09 12:28:00 -0300

    primeiro commit

--- src/cursoJava/Hello.java
+++ src/cursoJava/Hello.java
@@ -2,6 +2,6 @@ package cursoJava;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
