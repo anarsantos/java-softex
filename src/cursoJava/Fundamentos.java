17eb82a817b2f6a9d95f04dfd94bfa80b901984d
anarsantos <aninha_rsantos@yahoo.com.br>
2023-08-09 13:21:08 -0300

    tipos de variáveis

---
+++ src/cursoJava/Fundamentos.java
@@ -0,0 +1,17 @@
+package cursoJava;
+
+public class Fundamentos {
+    public static void main(String[] args) {
+        String nome; //outa forma de declarar a variável - String nome = "Terra";
+        int idade = 25;
+        char sexo = 'M';
+        double temperatura = 26.7;
+        boolean arCondicionado = false;
+        nome = "Sol e Lua";
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Temperatura: " + temperatura);
+        System.out.println("Ar condicionado: " + arCondicionado);
+    }
+}
